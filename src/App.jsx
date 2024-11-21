653d74330080f2ce36a32c8391d9498f9e0dfa90
LilaniRanjan <ranjanlilani@gmail.com>
2024-11-21 22:06:21 +0530

    Body Layout part added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Appbar from "./components/Appbar";
+import BodyLayout from "./components/bodyLayout";
 
 export default function App() {
   return (
     <>
       <Appbar/>
+      <BodyLayout/>
     </>
   )
 }

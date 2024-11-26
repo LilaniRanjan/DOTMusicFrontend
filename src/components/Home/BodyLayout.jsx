fcb126d9451f370b006c9fb8e2ae921095bc9f54
LilaniRanjan <ranjanlilani@gmail.com>
2024-11-26 21:51:01 +0530

    Artist section added

--- src/components/Home/BodyLayout.jsx
+++ src/components/Home/BodyLayout.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import BodyLeftLayout from './BodyLeftLayout';
+import BodyRightLayout from './BodyRightLayout';
 
 const BodyLayout = () => {
   return (
     <div className='grid grid-cols-12 gap-4 dark:bg-black'>
       {/* Left Sidebar */}
-      <div className='col-start-1 col-end-5 bg-gray-900 rounded-lg mx-2 pb-16 mb-8'>
+      <div className='col-start-1 col-end-5 bg-gray-900 rounded-lg ml-2 pb-16 mb-8'>
         <BodyLeftLayout/>
       </div>
     
       {/* Main Content */}
-      <div className='col-start-5 col-end-13 text-white'>
-        
+      <div className='col-start-5 col-end-13 text-white bg-gray-900 rounded-lg mr-2 mb-8'>
+        <BodyRightLayout/>
       </div>
     </div>
   );
